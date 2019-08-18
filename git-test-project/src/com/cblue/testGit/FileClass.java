3c7ea2290340f3011fd8601d73fb392870eb60c0
ggy <sunny2544@163.com>
2019-08-18 15:33:15 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/cblue/testGit/FileClass.java
@@ -0,0 +1,13 @@
+package com.cblue.testGit;
+
+public class FileClass {
+    public static void main(String[] args ){
+        System.out.println("Hello Test Git");
+        System.out.println("Hello Test Git");
+        System.out.println("Hello Test Git");
+        System.out.println("Hello Test Git");
+        System.out.println("Hello Test Git");
+        System.out.println("Hello Test Git");
+        System.out.println("Hello Test Git");
+    }
+}
