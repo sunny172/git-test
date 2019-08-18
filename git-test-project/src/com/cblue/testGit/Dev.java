6ce2cd6b9933264ffe7af8b38177ccbd28aabdba
ggy <sunny2544@163.com>
2019-08-18 16:32:48 +0800

    添加了一个Dev类

---
+++ git-test-project/src/com/cblue/testGit/Dev.java
@@ -0,0 +1,12 @@
+package com.cblue.testGit;
+
+public class Dev {
+    public static void main(String[] args ){
+        int round = Math.round(10);
+        double random = Math.random();
+        System.out.println(random);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(round);
+        }
+    }
+}
